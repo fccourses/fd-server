4e441728d272be834ac29e635ffaa9af2f331e43
Vladimir <freshcode.yt@gmail.com>
2021-03-11 17:05:05 +0200

    Add node server

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 const http = require('http')
+const fs = require('fs').promises
 
-let reqCount = 0
+const users = []
 
-const requestListener = (req, res) => {
-  console.log(++reqCount)
-  
-  res.end(
-    `HELLO WORLD SERVER NODE.JS 14.16.0 Your req number is ${reqCount}`
-  )
+const routerGET = {
+  '/': async (req, res) => {
+    const data = await fs.readFile('./views/index.html', { encoding: 'utf8' })
+    return res.end(data)
+  },
+  '/contacts.html': async (req, res) => {
+    const data = await fs.readFile('./views/contacts.html', {
+      encoding: 'utf8'
+    })
+    return res.end(data)
+  },
+  '/about.html': async (req, res) => {
+    const data = await fs.readFile('./views/about.html', { encoding: 'utf8' })
+    return res.end(data)
+  }
 }
 
-const server = http.createServer(requestListener)
+const routerPOST = {
+  '/signup': async (req, res) => {
+    let jsonString = ''
+    req.on('data', chunk => {
+      const strData = chunk.toString()
+      jsonString += strData
+    })
 
-server.listen(30000)
+    req.on('end', () => {
+      const user = JSON.parse(jsonString)
+      user.id = Date.now()
+      delete user.password
+      users.push(user)
+
+      console.log(users)
+    })
+
+    return res.end('user created')
+  }
+}
+
+const requestListener = async (req, res) => {
+  const { method, url } = req 
+
+  if (method === 'GET') {
+    return routerGET[url](req, res)
+  }
+
+  if (method === 'POST') {
+    
+    if (routerPOST[url]) {
+
+      return routerPOST[url](req, res)
+    }
+  }
+
+  res.end('404. not found page')
+}
+
+const server = http.createServer(requestListener) 
+
+server.listen(3000) //start server
